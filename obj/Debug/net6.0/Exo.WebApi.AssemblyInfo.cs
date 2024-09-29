e1eee695feaf7184ac05ad1e87f79d79d09b8265
Claudiney81 <149173560+Claudiney81@users.noreply.github.com>
2024-09-29 10:37:43 -0300

    terceiro commit

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1c480b79f9a1624bc2cfaca2b711269cf661d9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34af2d973ab900572e61211a704c265d1649062a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
